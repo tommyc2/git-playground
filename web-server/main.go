2dc1eceb7d10c05efdbcae05b1a85634ac95c14d
Tommy Condon <tcondon@redhat.com>
2025-01-22 15:45:51 +0000

    Add boiler plate for script

--- web-server/main.go
+++ web-server/main.go
@@ -5,9 +5,19 @@ import "fmt"
 func main() {
 	fmt.Println("Initializing web server......")
 	setup()
+	handleGETRequests()
+	handlePOSTRequests()
 }
 
 func setup() {
 	// setup code goes here
 	//  e.g. web server code
+}
+
+func handleGETRequests() {
+
+}
+
+func handlePOSTRequests() {
+
 }
